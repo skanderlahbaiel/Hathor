ec834fecfb9e62eeb5d36e05a9bcf84b58a45da6
unknown <skander.lahbaiel@medtech.tn>
2023-08-29 01:31:53 +0100

    Added function to stay focused on form

--- scripts/responsiveinput.js
+++ scripts/responsiveinput.js
@@ -1,31 +1,10 @@
-const inputFields = document.querySelectorAll('input, textarea, select');
-let isInputFocused = false;
+const selectElement = document.querySelector('select');
 
-// Add event listeners to focus and blur events of input fields
-inputFields.forEach((inputField) => {
-  inputField.addEventListener('focus', handleInputFocus);
-  inputField.addEventListener('blur', handleInputBlur);
+selectElement.addEventListener('focus', () => {
+  const currentViewportHeight = window.innerHeight;
+  document.documentElement.style.height = `${currentViewportHeight}px`;
 });
 
-function handleInputFocus() {
-  isInputFocused = true;
-  adjustViewportHeight();
-}
-
-function handleInputBlur() {
-  isInputFocused = false;
-  adjustViewportHeight();
-}
-
-function adjustViewportHeight() {
-  if (isInputFocused) {
-    // Set the viewport height to the window.innerHeight to prevent resizing
-    document.documentElement.style.height = `${window.innerHeight}px`;
-  } else {
-    // Reset the viewport height to 'auto' to allow normal resizing
-    document.documentElement.style.height = 'auto';
-  }
-}
-
-// Listen for window resize events and adjust viewport height accordingly
-window.addEventListener('resize', adjustViewportHeight);
+selectElement.addEventListener('blur', () => {
+  document.documentElement.style.height = 'auto';
+});
